c245e5360873deaba0592e45c033f110b92ae6b8
KAVASAKKI <81836951+KAVASAKKI@users.noreply.github.com>
2022-02-22 18:13:36 +0200

    Added "Statistics" task

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import Profile from 'components/Profile.js';
+import Profile from 'components/Profile/Profile.js';
+import Statistics from 'components/Statistics/Statistics';
 import user from './data/user.json';
+import stats from './data/data.json';
 
 export default function App() {
   return (
-    <div className="container">
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-    </div>
+    <>
+      <div className="container">
+        <Profile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+      </div>
+      <div className="container">
+        <Statistics items={stats} />
+      </div>
+    </>
   );
 }
---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,33 @@
+import PropsTypes from 'prop-types';
+import s from './Statistics.module.css';
+import randomColor from 'javascripts/random-color';
+
+export default function Statistics({ items }) {
+  return (
+    <section className={s.container}>
+      <h2 className={s.title}>Upload stats</h2>
+      <ul className={s.statsList}>
+        {items.map(item => (
+          <li
+            className={s.statsItem}
+            key={item.id}
+            style={{ backgroundColor: randomColor() }}
+          >
+            <span className={s.label}>{item.label}</span>
+            <span className={s.value}>{item.percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+Statistics.PropsTypes = {
+  items: PropsTypes.arrayOf({
+    item: PropsTypes.shape({
+      id: PropsTypes.string,
+      label: PropsTypes.string.isRequired,
+      percentage: PropsTypes.number.isRequired,
+    }),
+  }),
+};
