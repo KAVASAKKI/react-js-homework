271c896edd066f903d01223c5b87c5484fc1c8fa
KAVASAKKI <81836951+KAVASAKKI@users.noreply.github.com>
2022-02-22 22:23:59 +0200

    Added forgotten files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import Profile from 'components/Profile/Profile.js';
 import Statistics from 'components/Statistics/Statistics';
+import FriendList from 'components/FriendList/FriendList';
+import TransactionsHistory from 'components/TransactionsHistory/TransactionsHistory';
 import user from './data/user.json';
 import stats from './data/data.json';
+import friends from './data/friends.json';
+import transactions from './data/transactions.json';
 
 export default function App() {
   return (
@@ -15,8 +19,17 @@ export default function App() {
           stats={user.stats}
         />
       </div>
+
       <div className="container">
         <Statistics items={stats} />
+      </div>
+
+      <div className="container">
+        <FriendList friends={friends} />
+      </div>
+
+      <div className="container">
+        <TransactionsHistory items={transactions} />
       </div>
     </>
   );
