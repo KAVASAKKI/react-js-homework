b0af68c18326064b162db5033e3c00b0a1bdf3c0
Pavel Omelchenko <p.omel4enko2002@gmail.com>
2022-03-23 18:32:37 +0200

    Added local storage to phonebook

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
-import React from 'react';
-import Feedback from './bus/Feedback';
-import Phonebook from './bus/Phonebook';
+import Phonebook from './components/Phonebook';
 
 export default function App() {
   return (
     <>
-      <div className="container">
-        <Feedback />
-      </div>
-
       <div className="container">
         <Phonebook />
       </div>
--- src/components/Phonebook/index.jsx
+++ src/components/Phonebook/index.jsx
@@ -9,14 +9,23 @@ import {
 
 class Phonebook extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem('contacts'));
+    contacts && this.setState({ contacts });
+  }
+
+  componentDidUpdate(prevState) {
+    const prevContacts = prevState.contacts;
+    const nextContacts = this.state.contacts;
+
+    if (prevContacts !== nextContacts) {
+      localStorage.setItem('contacts', JSON.stringify(nextContacts));
+    }
+  }
 
   addContact = newContact => {
     const isExist = this.state.contacts.find(
