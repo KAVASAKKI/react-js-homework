957105c9b0be228afbeb000a8ff2250bcf4b280e
KAVASAKKI <81836951+KAVASAKKI@users.noreply.github.com>
2022-06-13 01:51:50 +0300

    Removed redux tools from production version, moved "react-redux" package

--- src/redux/store.js
+++ src/redux/store.js
@@ -32,6 +32,7 @@ export const store = configureStore({
     contacts: contactsReducer,
   },
   middleware,
+  devTools: process.env.NODE_ENV === 'development',
 });
 
 export const persistor = persistStore(store);
